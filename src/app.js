4f3bba51d7c9914df415f9394b940b759f3e81a1
s_ushizaki <s_ushizaki@ishg.co.jp>
2023-01-26 19:03:01 +0900

    環境構築（MySQLまだつなげてない）

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var hello = require('./routes/hello');
 
 var app = express();
 
@@ -18,9 +20,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+var session_opt = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 }
+};
+app.use(session(session_opt));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', hello);
+
+var boardsRouter = require('./routes/boards');
+app.use('/boards', boardsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,136 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { TimeoutError } = require('sequelize');
+const router = express.Router();
+const db = require('../models/index');
+//const { Op } = require("sequelize");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  //const nm = req.query.name
+  //const ml = req.query.mail
+  //const min = req.query.min * 1
+  //const max = req.query.max * 1
+  db.User.findAll(
+    //{
+    // where: {
+    //   //age: { [Op.gte]:min, [Op.lte]:max}
+    //   [Op.or]: [
+    //     {name: {[Op.like]:'%'+nm+'%'}},
+    //     {mail: {[Op.like]:'%'+ml+'%'}}
+    //   ]    
+    // }
+  //}
+  ).then(usrs => {
+    var data = {
+      title: 'Users/Index',
+      content: usrs
+    }
+    res.render('users/index', data);
+  });
+});
+
+router.get('/add', (req, res, next) => {
+  var data = {
+    title: 'Users/Add',
+    form: new db.User(),
+    err: null
+  }
+  res.render('users/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+  const form = {
+    name: req.body.name,
+    pass: req.body.pass,
+    mail: req.body.mail,
+    age: req.body.age
+  }
+  db.sequelize.sync()
+    .then(() => db.User.create(form)
+    .then(usr => {
+      res.redirect('/users');
+    })
+    .catch(err=> {
+      var data = {
+      title: 'Users/Add',
+      form: new db.User(),
+      err: err
+    }
+    res.render('users/add', data);
+  })
+  )
+});
+
+router.get('/edit',(req, res, next)=> {
+  db.User.findByPk(req.query.id)
+  .then(usr => {
+    var data = {
+      title: 'Users/Edit',
+      form: usr
+    }
+    res.render('users/edit', data);
+  });
+});
+
+router.post('/edit',(req, res, next)=> {
+  db.User.findByPk(req.query.id)
+  .then(usr => {
+    usr.name = req.body.name;
+    usr.pass = req.body.pass;
+    usr.mail = req.body.mail;
+    usr.age = req.body.age;
+    usr.save().then(()=>res.redirect('/users'));
+  });
+});
+
+router.get('/delete',(req, res, next)=> {
+  db.User.findByPk(req.query.id)
+  .then(usr => {
+    var data = {
+      title: 'Users/Delete',
+      form: usr
+    }
+    res.render('users/delete', data);
+  });
+});
+
+router.post('/delete',(req, res, next)=> {
+  db.User.findByPk(req.body.id)
+  .then(usr => {
+    usr.destroy().then(() => res.redirect('/users'));
+  });
+});
+
+router.get('/login', (req, res, next) => {
+  var data = {
+    title: 'Users/Login',
+    content:'名前とパスワードを入力して下さい。'
+  }
+  res.render('users/login', data);
+});
+
+router.post('/login', (req, res, next) => {
+  db.User.findOne({
+    where:{
+      name:req.body.name,
+      pass:req.body.pass,
+    }
+  }).then(usr=>{
+    if (usr != null) {
+      req.session.login = usr;
+      let back = req.session.back;
+      if (back == null){
+        back = '/';
+      }
+      res.redirect(back);
+    } else {
+      var data = {
+        title:'Users/Login',
+        content:'名前かパスワードに問題があります。再度入力下さい。'
+      }
+      res.render('users/login', data);
+    }
+  })
 });
 
 module.exports = router;
