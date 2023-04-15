13342d6f88a895193af501cb30e040553ef6a9f4
Satoshi Ushizaki <uruteximax1@au.com>
2023-04-15 16:18:41 +0900

    一日一回しか出退勤できないように修正＆分表示を正しく修正

--- src/routes/attendance.js
+++ src/routes/attendance.js
@@ -5,6 +5,8 @@ const moment = require('moment');
 const { Op } = require("sequelize");
 
 
+const today = moment().format('YYYY-MM-DD 00:00:00');
+
 //ログインのチェック
 function check(req,res) {
   if (req.session.login == null) {
@@ -14,11 +16,10 @@ function check(req,res) {
   } else {
     return false;
   }
-}
+};
 
 // 出退勤ページ
 router.get('/',(req, res, next)=> {
-  var today = moment().format('YYYY-MM-DD 00:00:00');
   db.Attendance.findAll({
     attributes: ['begin_time', 'finish_time'],
     where:{
@@ -26,62 +27,109 @@ router.get('/',(req, res, next)=> {
       updatedAt: {[Op.gt]: today}
     }
   }).then(db_data=>{
-    console.log(db_data);
-    var beginTime = db_data[0].dataValues.begin_time;
-    var begin = new Date(beginTime)
-    var begin_time = begin.getHours() + ':' + begin.getMinutes();
-    if (db_data[1]) {
-      var finishTime = db_data[1].dataValues.finish_time;
-      var finish = new Date(finishTime)
-      var finish_time = finish.getHours() + ':' + finish.getMinutes();
-    }
+    if (db_data[0]) {
+      var beginTime = db_data[0].dataValues.begin_time;
+      var begin = new Date(beginTime);
+      var beginMinutes = begin.getMinutes().toString().padStart(2, '0');
+      var begin_time = begin.getHours() + ':' + beginMinutes;
+      if (db_data[0].dataValues.finish_time) {
+        var finishTime = db_data[0].dataValues.finish_time;
+        var finish = new Date(finishTime)
+        var finishMinutes = finish.getMinutes().toString().padStart(2, '0');
+      var finish_time = finish.getHours() + ':' + finishMinutes;
+      }
+    };
+    var message = message ? message : '';
     var data = {
       title: '出退勤画面',
       login: req.session.login,
       begin_time: begin_time,
-      finish_time: finish_time
+      finish_time: finish_time,
+      message: message
     }
     res.render('attendance/index', data);
-  });
+  })
 });
 
 // 出勤時間の送信処理
 router.post('/begin',(req, res, next)=> {
-  var now_time = moment().format('YYYY-MM-DD HH:mm:ss');
-  var data = {
-    begin_time: now_time
-  }
   if (check(req,res)){ return };
-  db.sequelize.sync()
-    .then(() => db.Attendance.create({
-      userId: req.session.login.id,
-      begin_time: now_time
-    })
-    .then(atd=>{
-      res.redirect('/attendance', data);
-    })
-    .catch(err=>{
-      res.redirect('/attendance');
-    })
+  const begin_time = req.body.begin_time;
+  const finish_time = req.body.finish_time;
+  if (begin_time) {
+    const message = '※本日はすでに出勤打刻済みです！'
+    var data = {
+      title: '出退勤画面',
+      login: req.session.login,
+      begin_time: begin_time,
+      finish_time: finish_time,
+      message: message
+    }
+    res.render('attendance/index', data);
+  } else {
+    const now_time = moment().format('YYYY-MM-DD HH:mm:ss');
+    db.sequelize.sync()
+      .then(() => db.Attendance.create({
+        userId: req.session.login.id,
+        begin_time: now_time
+      })
+      .then(atd=>{
+        res.redirect('/attendance');
+      })
+      .catch(err=>{
+        res.redirect('/attendance');
+      })
     )
+  }
 });
+  
 
 // 退勤時間の送信処理
 router.post('/finish',(req, res, next)=> {
-  var finish_time = moment().format('YYYY-MM-DD HH:mm:ss');
   if (check(req,res)){ return };
-  db.sequelize.sync()
-    .then(() => db.Attendance.create({
-      userId: req.session.login.id,
-      finish_time: finish_time
-    })
-    .then(atd=>{
-      res.redirect('/attendance', finish_time);
-    })
-    .catch(err=>{
-      res.redirect('/attendance');
-    })
+  const begin_time = req.body.begin_time;
+  const finish_time = req.body.finish_time;
+  if (!begin_time) {
+    const message = '※本日はまだ出勤打刻していません！'
+    var data = {
+      title: '出退勤画面',
+      login: req.session.login,
+      begin_time: begin_time,
+      finish_time: finish_time,
+      message: message
+    }
+    res.render('attendance/index', data);
+  } else if (finish_time) {
+    const message = '※本日はすでに退勤打刻済みです！'
+    var data = {
+      title: '出退勤画面',
+      login: req.session.login,
+      begin_time: begin_time,
+      finish_time: finish_time,
+      message: message
+    }
+    res.render('attendance/index', data);
+  } else {
+    const now_time = moment().format('YYYY-MM-DD HH:mm:ss');
+    db.sequelize.sync()
+      .then(() => db.Attendance.update({
+        finish_time: now_time
+      },
+      {
+        where:{
+        userId: req.session.login.id,
+        updatedAt: {[Op.gt]: today}
+      }
+       
+      })
+      .then(atd=>{
+        res.redirect('/attendance');
+      })
+      .catch(err=>{
+        res.redirect('/attendance');
+      })
     )
+  }
 });
 
 
